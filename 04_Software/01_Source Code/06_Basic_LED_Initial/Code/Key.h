0f3f838c015928d43e121e5ca23ac14d5687c900
ben black <KNBSGR@outlook.com>
2025-02-26 21:55:21 +0800

    Readme

--- 04_Software/01_Source Code/06_Basic_LED_Initial/Code/Key.h
+++
@@ -1,6 +0,0 @@
-#ifndef __KEY_H__
-#define __KEY_H__
-
-#include "main.h"
-uint8_t Key_Scan(void);
-#endif
--- 04_Software/01_Source Code/06_Basic_LED_Initial/Code/Led.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LED_H__
-#define __LED_H__
-
-#include "main.h"
-
-void LED_OFF(void);
-void LED_ON(void);
-
-#endif
